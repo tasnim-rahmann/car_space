b3683d5b6c2873d7b5a1354cf81ed04362d4265c
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-09-14 17:46:50 +0600

    design navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const NavBar = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <div className="navbar py-4">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-medium">
+                            <li><a>Home</a></li>
+                            <li><a>Aobut</a></li>
+                            <li><a>Services</a></li>
+                            <li><a>Blog</a></li>
+                            <li><a>Contact</a></li>
+                        </ul>
+                    </div>
+                    <Link href={"/"}>
+                        <Image src={"/assets/logo.svg"} width={60} height={60} />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 font-medium">
+                        <li><a>Home</a></li>
+                        <li><a>Aobut</a></li>
+                        <li><a>Services</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn text-orange-500 border-orange-500 rounded-sm">Appointment</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
